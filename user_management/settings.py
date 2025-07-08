c951b34c7c60aa1e8aa1a7b05b67f16fb5ad2462
Zablon Zambagarrah <132018635+Zambagarrah@users.noreply.github.com>
2025-07-08 22:48:09 +0300

    Add password reset flow with styled templates

--- user_management/settings.py
+++ user_management/settings.py
@@ -50,6 +50,9 @@ ALLOWED_HOSTS = [
     'django-user-management-system.onrender.com',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
